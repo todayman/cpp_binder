f0cb4f765a1e4d0c282d4657d37469cb242638e9
Paul O'Neil <redballoon36@gmail.com>
2014-05-07 01:09:59 -0400

    Use a different API to retain ownership of the ASTContext after the traversal has finished.
    The goal is to build a queryable AST structure, then do a second pass
    with analysis, and a final pass to generate output.

--- main.cpp
+++ main.cpp
@@ -1,9 +1,14 @@
 #include "clang/AST/ASTConsumer.h"
 #include "clang/AST/ASTContext.h"
 #include "clang/AST/RecursiveASTVisitor.h"
+#include "clang/Frontend/ASTUnit.h"
 #include "clang/Frontend/CompilerInstance.h"
 #include "clang/Frontend/FrontendAction.h"
 #include "clang/Tooling/Tooling.h"
+
+#include <fstream>
+#include <iostream>
+#include <memory>
 
 using namespace clang;
 
@@ -58,10 +63,27 @@ class FindNamedClassAction : public clang::ASTFrontendAction
     }
 };
 
+std::string readFile(const std::string& filename)
+{
+    std::ifstream input(filename.c_str());
+    size_t length;
+    input.seekg(0, std::ios::end);
+    length = input.tellg();
+    input.seekg(0, std::ios::beg);
+    std::string result;
+    result.resize(length);
+    input.read(&result[0], length);
+    return result;
+}
+
 int main(int argc, const char **argv)
 {
     if( argc > 1 )
     {
-        clang::tooling::runToolOnCode(new FindNamedClassAction, argv[1]);
+        std::string contents = readFile(argv[1]);
+        std::vector<std::string> clang_args;
+        std::shared_ptr<ASTUnit> ast(clang::tooling::buildASTFromCodeWithArgs(contents, clang_args, argv[1]));
+        FindNamedClassConsumer consumer(&ast->getASTContext());
+        consumer.HandleTranslationUnit(ast->getASTContext());
     }
 }
