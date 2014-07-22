9b456741b687a3f68bdb626a7e50827db087524d
Paul O'Neil <redballoon36@gmail.com>
2014-07-21 21:14:04 -0400

    Rename clang_ast_visitor.* -> cpp_decl.*

--- source/cpp_decl.hpp
+++ source/cpp_decl.hpp
@@ -4,6 +4,8 @@
 #include "clang/AST/Decl.h"
 #include "clang/AST/RecursiveASTVisitor.h"
 
+#include "cpp_exception.hpp"
+
 namespace cpp
 {
     // Same thing as Type, but for declarations of functions,
@@ -57,6 +59,72 @@ DECLARATION_CLASS_2(CXXDestructor, Destructor);
 DECLARATION_CLASS_2(ParmVar, Argument);
 DECLARATION_CLASS_2(Var, Variable);
 
+    // TODO invalid decl class used to fill in things that are unwrappable,
+    // so we know to properly deal with that later (i.e. leaving vtable entries,
+    // exposing all of one class except for a non-virtual method, or something)
+    class DeclVisitor : public clang::RecursiveASTVisitor<DeclVisitor>
+    {
+        private:
+        template<class SourceType, class TargetType>
+        void allocateDeclaration(SourceType * decl) {
+            decl_in_progress = std::make_shared<TargetType>(decl);
+            declarations.insert(std::make_pair(decl, decl_in_progress));
+        }
+
+        bool registerDeclaration(clang::Decl* cppDecl);
+
+        static std::unordered_map<clang::Decl*, std::shared_ptr<Declaration>> declarations;
+
+        std::shared_ptr<Declaration> decl_in_progress;
+
+        bool TraverseDeclContext(clang::DeclContext * cpp_context);
+        public:
+        typedef clang::RecursiveASTVisitor<DeclVisitor> Super;
+
+        DeclVisitor();
+
+        bool TraverseType(clang::QualType type)
+        {
+            Type::get(type);
+            return true;
+        }
+
+        bool TraverseDecl(clang::Decl * Declaration);
+        bool TraverseTranslationUnitDecl(clang::TranslationUnitDecl* cppDecl);
+        bool TraverseFunctionDecl(clang::FunctionDecl* cppDecl);
+        bool TraverseClassTemplatePartialSpecializationDecl(clang::ClassTemplatePartialSpecializationDecl* declaration);
+        bool TraverseCXXMethodDecl(clang::CXXMethodDecl* Declaration);
+        bool TraverseCXXConstructorDecl(clang::CXXConstructorDecl* Declaration);
+        bool TraverseCXXDestructorDecl(clang::CXXDestructorDecl* Declaration);
+        bool TraverseLinkageSpecDecl(clang::LinkageSpecDecl* cppDecl);
+        bool TraverseEnumDecl(clang::EnumDecl* cppDecl);
+        //bool TraverseEnumConstantDecl(clang::EnumConstantDecl* cppDecl);
+        // TODO handle constexpr
+        bool TraverseVarDecl(clang::VarDecl* cppDecl);
+
+        bool WalkUpFromDecl(clang::Decl* cppDecl);
+        bool WalkUpFromTranslationUnitDecl(clang::TranslationUnitDecl* cppDecl);
+        bool WalkUpFromFunctionDecl(clang::FunctionDecl * Declaration);
+        bool WalkUpFromTypedefDecl(clang::TypedefDecl * decl);
+        bool WalkUpFromNamespaceDecl(clang::NamespaceDecl * cppDecl);
+        bool WalkUpFromCXXMethodDecl(clang::CXXMethodDecl* cppDecl);
+        bool WalkUpFromCXXConstructorDecl(clang::CXXConstructorDecl* cppDecl);
+        bool WalkUpFromCXXDestructorDecl(clang::CXXDestructorDecl* cppDecl);
+        bool WalkUpFromParmVarDecl(clang::ParmVarDecl* cppDecl);
+        bool WalkUpFromRecordDecl(clang::RecordDecl* cppDecl);
+        bool WalkUpFromEnumDecl(clang::EnumDecl* cppDecl);
+        bool WalkUpFromEnumConstantDecl(clang::EnumConstantDecl* cppDecl);
+        bool WalkUpFromVarDecl(clang::VarDecl* cppDecl);
+
+        // Also gets cxx method/ctor/dtor
+        bool VisitFunctionDecl(clang::FunctionDecl * Declaration);
+        bool VisitTypedefDecl(clang::TypedefDecl * decl);
+        bool VisitParmVarDecl(clang::ParmVarDecl* cppDecl);
+        bool VisitNamedDecl(clang::NamedDecl* cppDecl);
+    };
+
 } // namespace cpp
+
+extern const clang::SourceManager * source_manager;
 
 #endif // __CPP_DECL_HPP__
