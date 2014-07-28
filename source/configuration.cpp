7856b60b03cf4ea9eeb43330803c717251b7e972
Paul O'Neil <redballoon36@gmail.com>
2014-07-27 22:28:46 -0400

    Put the yajl_val in a shared_ptr and error if not an object

--- source/configuration.cpp
+++ source/configuration.cpp
@@ -5,6 +5,16 @@
 
 #include "configuration.hpp"
 
+class ExpectedObject : ConfigurationException
+{
+    public:
+    // TODO incorporate the value (with context)
+    // into the error message.
+    ExpectedObject(std::shared_ptr<yajl_val_s>)
+        : ConfigurationException("Expected JSON object, got something like number, array, etc.")
+    { }
+};
+
 std::string readFile(const std::string& filename)
 {
     std::ifstream input(filename.c_str());
@@ -24,14 +34,18 @@ static void applyConfigFromFile(const std::string& filename, clang::ASTContext& 
     constexpr size_t BUFFER_SIZE = 512;
     char error_buffer[BUFFER_SIZE];
 
-    yajl_val tree_root = yajl_tree_parse(config_contents.c_str(), error_buffer, BUFFER_SIZE);
-    if( !tree_root )
+    yajl_val tree_root_raw = yajl_tree_parse(config_contents.c_str(), error_buffer, BUFFER_SIZE);
+    if( !tree_root_raw )
     {
         error_buffer[BUFFER_SIZE - 1] = 0; // FIXME I don't know if I need that
         throw ConfigurationException(error_buffer);
     }
+    std::shared_ptr<yajl_val_s> tree_root(tree_root_raw, yajl_tree_free);
 
-    yajl_tree_free(tree_root);
+    if( !YAJL_IS_OBJECT(tree_root) )
+    {
+        throw ExpectedObject(tree_root);
+    }
 }
 
 void parseAndApplyConfiguration(const std::vector<std::string>& config_files, clang::ASTContext& ast)
