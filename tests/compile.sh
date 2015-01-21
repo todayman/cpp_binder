289148746e733591a552cb3fa6a584f2f935977c
Paul O'Neil <redballoon36@gmail.com>
2015-01-21 01:51:36 -0500

    Replace tests/compile.sh with dub!

--- tests/compile.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/sh
-DPARSE=~/Documents/d_libs/libdparse
-
-dmd -g \
-    -I~${DPARSE}/src \
-    ${DPARSE}/src/std/d/*.d \
-    ${DPARSE}/src/std/*.d \
-    test_runner.d \
-    -oftest_runner
