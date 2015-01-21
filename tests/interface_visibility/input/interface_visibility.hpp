7f280bb315f0ccd0b27512724bcbc4285b1d69a0
Paul O'Neil <redballoon36@gmail.com>
2015-01-21 02:13:52 -0500

    Make sure that attributes in interfaces are output correctly

---
+++ tests/interface_visibility/input/interface_visibility.hpp
@@ -0,0 +1,9 @@
+struct Simple
+{
+    private:
+    virtual int a();
+    protected:
+    virtual char b();
+    public:
+    virtual float c();
+};
---
+++ tests/interface_visibility/output/unknown.d
@@ -0,0 +1,8 @@
+module unknown;
+
+extern(C++) interface Simple
+{
+    private int a();
+    protected char b();
+    public float c();
+}
