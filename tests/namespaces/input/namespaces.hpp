67f8be02af65b4acdcecc08253515ca5b4321b40
Paul O'Neil <redballoon36@gmail.com>
2015-03-23 21:05:36 -0400

    Add the namespaces test

---
+++ tests/namespaces/input/namespaces.hpp
@@ -0,0 +1,14 @@
+namespace A
+{
+    int func_a();
+}
+
+namespace B
+{
+    bool func_b();
+}
+
+namespace A
+{
+    void func_c();
+}
---
+++ tests/namespaces/output/unknown.d
@@ -0,0 +1,13 @@
+module unknown;
+
+extern(C++, A)
+{
+    int func_a();
+
+    void func_c();
+}
+
+extern(C++, B)
+{
+    bool func_b();
+}
