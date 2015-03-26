36539ad21b76ff15f69a2df4dc21cad1c82b49d6
Paul O'Neil <redballoon36@gmail.com>
2015-03-25 21:06:48 -0400

    Get rid of extern(C++) inside a struct body

--- tests/dependent_types/output/unknown.d
+++ tests/dependent_types/output/unknown.d
@@ -3,7 +3,7 @@ module unknown;
 extern(C++) struct Template(T)
 {
     public T var;
-    extern(C++) alias MyType = T;
+    alias MyType = T;
 }
 
 extern(C++) extern unknown.Template!(int).MyType var;
